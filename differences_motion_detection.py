8c91063cbe2fd88619c52a4409c8baba81ae55de
ItaySamuelov <itaysamuelov@campus.technion.ac.il>
2025-05-02 10:56:11 +0300

    Update differences_motion_detection.py
    section of setting the relevant parameters for the motion detection algorithm

--- differences_motion_detection.py
+++ differences_motion_detection.py
@@ -1,3 +1,9 @@
 import cv2
 import numpy as np
 import os
+
+# --- Parameters ---
+video_path = r'C:\Users\Bar\Desktop\Itay\Hawkeye\videos\Shapes1.mp4'
+k = 5           # number of frames back to compare
+thresh = 25     # motion threshold
+window_name = f"{k}-Step Motion Detection"

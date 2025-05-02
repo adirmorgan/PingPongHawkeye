00201408c8191e0055c4074e090629c191b6a3e4
ItaySamuelov <itaysamuelov@campus.technion.ac.il>
2025-05-02 11:30:04 +0300

    Update differences_motion_detection.py
    changing the video path from my local computer to our GitHub's Data folder

--- differences_motion_detection.py
+++ differences_motion_detection.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 # --- Parameters ---
-video_path = r'C:\Users\Bar\Desktop\Itay\Hawkeye\videos\Shapes1.mp4'
+video_path = r'./Data/sim_data/Shapes1.mp4'
 k = 5           # number of frames back to compare
 thresh = 25     # motion threshold
 window_name = f"{k}-Step Motion Detection"

d5d0facd87be2bf89ea78b80f8bd672257553f57
ItaySamuelov <itaysamuelov@campus.technion.ac.il>
2025-05-02 11:02:55 +0300

    Update differences_motion_detection.py
    loading the video from the path specified on tne parameters

--- differences_motion_detection.py
+++ differences_motion_detection.py
@@ -7,3 +7,9 @@ video_path = r'C:\Users\Bar\Desktop\Itay\Hawkeye\videos\Shapes1.mp4'
 k = 5           # number of frames back to compare
 thresh = 25     # motion threshold
 window_name = f"{k}-Step Motion Detection"
+
+# --- Setup ---
+cap = cv2.VideoCapture(video_path)
+frame_buffer = []
+
+cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
